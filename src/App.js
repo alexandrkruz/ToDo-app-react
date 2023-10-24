e9585a99ebf9b0561a68141fd6e11c25962bab82
Alexandr <alexandrkruz76@gmail.com>
2023-10-24 19:18:19 +0300

    add-reat-router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,47 @@
 import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import TaskForm from './components/TaskForm/TaskForm';
 import TaskList from './components/TaskList';
 import SearchTask from './components/SearchTask/SearchTask';
 
 import './App.css';
+
+const Home = () => {
+  return (
+    <div>
+      <h2>Домашняя страница</h2>
+      <p>Добро пожаловать! Вы можете создать новую задачу или просмотреть существующие.</p>
+    </div>
+  );
+};
+
+const CreateTask = ({ handleTaskCreate }) => {
+  return (
+    <div className="options">
+      <h2 className="options_title">Управление задачами</h2>
+      <TaskForm onTaskCreate={handleTaskCreate} />
+    </div>
+  );
+};
+
+const TaskListView = ({ tasks, handleStatusChange, handleDeleteTask, updateTasks, searchText, handleSearchChange }) => {
+  const filteredTasks = tasks.filter((task) =>
+    task.name.toLowerCase().includes(searchText.toLowerCase())
+  );
+
+  return (
+    <div className="task_list">
+      <h2 className="list_title">Список Задач</h2>
+      <SearchTask onSearchChange={handleSearchChange} />
+      <TaskList
+        tasks={filteredTasks}
+        onStatusChange={handleStatusChange}
+        onDeleteTask={handleDeleteTask}
+        updateTasks={updateTasks}
+      />
+    </div>
+  );
+};
 
 function App() {
   const [tasks, setTasks] = useState([]);
@@ -30,31 +68,39 @@ function App() {
     setSearchText(text);
   };
 
-  const filteredTasks = tasks
-    .filter((task) => task.name.toLowerCase().includes(searchText.toLowerCase()));
-
   return (
-    <div className="App">
-      <div className="container">
-        <div className="options">
-          <h2 className="options_title">Управление задачами</h2>
-          <TaskForm onTaskCreate={handleTaskCreate} />
-        </div>
-
-        <div className="task_list">
-        <h2 className="list_title">Список Задач</h2>
-          <SearchTask onSearchChange={handleSearchChange} />
-          <TaskList
-            tasks={filteredTasks}
-            onStatusChange={handleStatusChange}
-            onDeleteTask={handleDeleteTask}
-            updateTasks={setTasks}
-          />
+    <Router>
+      <div className="App">
+        <div className="container">
+          <nav>
+            <Link to="/">Home</Link>
+            <Link to="/create">Create Task</Link>
+            <Link to="/view">View tasks</Link>
+          </nav>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route
+              path="/create"
+              element={<CreateTask handleTaskCreate={handleTaskCreate} />}
+            />
+            <Route
+              path="/view"
+              element={
+                <TaskListView
+                  tasks={tasks}
+                  handleStatusChange={handleStatusChange}
+                  handleDeleteTask={handleDeleteTask}
+                  updateTasks={setTasks}
+                  searchText={searchText}
+                  handleSearchChange={handleSearchChange}
+                />
+              }
+            />
+          </Routes>
         </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
 export default App;
-
